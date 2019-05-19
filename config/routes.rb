88e826497de57778838283fb7c4faae0deef3afc
Rafael <rreynoud@gmail.com>
2019-05-19 09:19:16 -0300

    funcionando  com a migração

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
 
+
   root to: "static_pages#index"
 
   get 'sobre', to:"static_pages#sobre"
 
   get 'contato', to:"static_pages#contato"
+
+  resources :contacts
+  resources :users, only: [:new, :create]
 
 end
